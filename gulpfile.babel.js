1ab635210c25ca65fb1fd5eb225e9eafe0660069
Amila Welihinda <amilajack@gmail.com>
2016-01-03 12:21:30 -0800

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+// Build script for notie.js
+
+'use strict';
+
+import gulp from 'gulp';
+import del from 'del';
+import path from 'path';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+// Default task
+gulp.task('scripts', () => {
+
+  gulp.src('./src/*')
+    .pipe($.concat('notie.min.js'))
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.sourcemaps.write())
+    .pipe($.uglify())
+    // .pipe($.uglify({preserveComments: 'license'}))
+    .pipe($.size({title: 'scripts'}))
+    .pipe(gulp.dest('./dist'))
+});
+
+// Watch scripts
+gulp.task('serve', () => {
+  gulp.watch([
+    './src/*',
+  ], ['scripts']);
+});
+
+// Clean output directory
+gulp.task('clean', cb => del([
+  './dist/*', // '!./public/dist/vendor'
+], {dot: true}));
+
+
+gulp.task('default', ['scripts']);
